63ff0a1f29a6b3e89d43e90d84a7ec407cbf45c0
Acer2 <Acer2>
2023-10-16 19:37:07 -0400

    create up/down motions responding to switch press

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -14,9 +14,32 @@ public class ClimberSubsystem extends SubsystemBase {
  private final DigitalInput topLimitswitch = new DigitalInput(1);
  private final DigitalInput bottomLimitswitch = new DigitalInput(0);
 
+/*   public void [functionName](args){
+  ajf;sldkfjasldkjf
+}
+*/
+// Public void elevatorUP
+
   public ClimberSubsystem() {}
 
-  @Override
+// Method to move the elevator up if it doesn't hit switch.
+public void elevatorUp(){
+  if(!topLimitswitch.get() == true){
+      elevatorMotor.set(0);
+    } else {
+      elevatorMotor.set(-1);
+    }
+  }
+
+// Method to move the elevator down if it hits switch.
+public void elevatorDown(){
+  if (!bottomLimitswitch.get() == true){
+    elevatorMotor.set(0);
+  } else {
+      elevatorMotor.set(1);
+    }
+  }
+  
   public void periodic() {
     // This method will be called once per scheduler run
   }
