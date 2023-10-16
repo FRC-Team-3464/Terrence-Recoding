cdb87280585282bead2c608b893e67f31369b6cc
Acer2 <Acer2>
2023-10-16 19:44:22 -0400

    create return function for switch states

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new ClimberSubsystem. */
  private final Spark elevatorMotor = new Spark(2);
- private final DigitalInput topLimitswitch = new DigitalInput(1);
- private final DigitalInput bottomLimitswitch = new DigitalInput(0);
+ private final DigitalInput topLimitSwitch = new DigitalInput(1);
+ private final DigitalInput bottomLimitSwitch = new DigitalInput(0);
 
 /*   public void [functionName](args){
   ajf;sldkfjasldkjf
@@ -24,7 +24,7 @@ public class ClimberSubsystem extends SubsystemBase {
 
 // Method to move the elevator up if it doesn't hit switch.
 public void elevatorUp(){
-  if(!topLimitswitch.get() == true){
+  if(!topLimitSwitch.get() == true){
       elevatorMotor.set(0);
     } else {
       elevatorMotor.set(-1);
@@ -33,13 +33,23 @@ public void elevatorUp(){
 
 // Method to move the elevator down if it hits switch.
 public void elevatorDown(){
-  if (!bottomLimitswitch.get() == true){
+  if (!bottomLimitSwitch.get() == true){
     elevatorMotor.set(0);
   } else {
       elevatorMotor.set(1);
     }
   }
-  
+
+  // Return top and bottom switch states (true/false)
+public boolean bottomSwitchState(){
+  return bottomLimitSwitch.get();
+} 
+
+public boolean topSwitchState(){
+  return topLimitSwitch.get();
+} 
+
+
   public void periodic() {
     // This method will be called once per scheduler run
   }
